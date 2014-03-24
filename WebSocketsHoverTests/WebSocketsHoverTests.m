a0d47268a93eeb32b711915ceeb1eaf076e489a1
Erick Noriega <noriega0893@gmail.com>
2014-03-24 14:43:25 -0600

    Cambios:

---
+++ WebSocketsHoverTests/WebSocketsHoverTests.m
@@ -0,0 +1,34 @@
+//
+//  WebSocketsHoverTests.m
+//  WebSocketsHoverTests
+//
+//  Created by Erick N on 3/21/14.
+//  Copyright (c) 2014 Erick N. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebSocketsHoverTests : XCTestCase
+
+@end
+
+@implementation WebSocketsHoverTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
